0e586d5bb7a89a747aa486cd20ae87cb773e648b
DeveloperLVA <DeveloperLVA@gmail.com>
2021-04-30 00:19:16 +0300

    Updates

---
+++ precache-manifest.727905f644c4b9073e60ebaadb73742b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b65c515ce7dcdea6699032b792aeb7",
+    "url": "/demo-samurai/index.html"
+  },
+  {
+    "revision": "72cf1a3f8b252341eb5d",
+    "url": "/demo-samurai/static/css/3.b27e7ae3.chunk.css"
+  },
+  {
+    "revision": "72902741a01206e31bda",
+    "url": "/demo-samurai/static/css/main.4982ed25.chunk.css"
+  },
+  {
+    "revision": "52191770b24e7058d3c5",
+    "url": "/demo-samurai/static/js/2.b5544458.chunk.js"
+  },
+  {
+    "revision": "9ca69d7bdeacd27130e2a604cea8f65b",
+    "url": "/demo-samurai/static/js/2.b5544458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cf1a3f8b252341eb5d",
+    "url": "/demo-samurai/static/js/3.71bbeda9.chunk.js"
+  },
+  {
+    "revision": "5ce45766659c79902d15",
+    "url": "/demo-samurai/static/js/4.6c253c05.chunk.js"
+  },
+  {
+    "revision": "72902741a01206e31bda",
+    "url": "/demo-samurai/static/js/main.4ebb50bc.chunk.js"
+  },
+  {
+    "revision": "b09ec432244942044a40",
+    "url": "/demo-samurai/static/js/runtime-main.9b882d1f.js"
+  },
+  {
+    "revision": "e9b43964e5fae7d7a033a1d65887520c",
+    "url": "/demo-samurai/static/media/Spinner-1s-200px.e9b43964.svg"
+  },
+  {
+    "revision": "8d15f1e7dcaf4bb7aa9772bf3939c584",
+    "url": "/demo-samurai/static/media/user.8d15f1e7.svg"
+  }
+]);
